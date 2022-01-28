e0b1503e7a6071a0fbe7e7bb237e905a9e567974
meriem lachhab <meriemlachhab58@gmail.com>
2022-01-28 10:39:44 +0100

    jj

--- include/script.php
+++ include/script.php
@@ -5,8 +5,11 @@
 	<script src="../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
 	<script src="../assets/vendor_components/moment/min/moment.min.js"></script>
 	<script src="../assets/vendor_components/bootstrap-daterangepicker/daterangepicker.js"></script>	
+	<script src="../src/js/pages/chat-popup.js"></script>
 	
 	<!-- edulearn App -->
-	<script src="../src/js/demo.js"></script>
+	 <script src="../src/js/demo.js"></script> 
 	<script src="../src/js/template.js"></script>
-	<script src="../src/js/pages/dashboard.js"></script>
+    <script src="../assets/icons/feather-icons/feather.min.js"></script>	
+	<script src="../assets/vendor_components/datatable/datatables.min.js"></script>
+	<script src="../src/js/pages/data-table.js"></script> 
